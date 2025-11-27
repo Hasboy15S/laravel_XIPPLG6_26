030b1b397a8344a2b07012bdcbeba7b8c06fcb2f
unknown <hasbigd.10@gmail.com>
2025-11-27 12:54:06 +0700

    Fix syntax error in web.php

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,11 @@ use App\Http\Controllers\AttendanceController;
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 Route::prefix('admin')->name('admin.')->group(function(){
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-    Route::resource('students', StudentController::class);
+    Route::resource('student', StudentController::class);
 Route::get('/absensi', [AttendanceController::class, 'index'])->name('absensi.index');
 Route::post('/absensi', [AttendanceController::class, 'store'])->name('absensi.store');
 Route::get('/absensi/riwayat', [AttendanceController::class, 'riwayat'])->name('absensi.riwayat');
 
 });
-
 
 
